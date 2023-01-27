75ed1ac4b34e40d8834505459cb0dd4868290a89
Marcello <marcello_gp_@hotmail.com>
2023-01-27 17:50:45 -0300

    created pricetag feature

---
+++ src/components/price-tag/index.js
@@ -0,0 +1,2 @@
+import PriceTag from './Price';
+export default PriceTag;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,14 @@
 import Gallery from '../../components/gallery';
+import PriceTag from '../../components/price-tag';
 import * as S from './style';
 
 export default function Home() {
   return (
     <S.ProductSection>
       <Gallery />
-      <div></div>
+      <div>
+        <PriceTag originalPrice={1000} discount={50} />
+      </div>
     </S.ProductSection>
   );
 }
